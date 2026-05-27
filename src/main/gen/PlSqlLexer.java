b45e4017ab1870accef8f8ea7126429cd24486aa
Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>
2026-05-27 08:16:12 +0200

    updated gradle submit to repos, added plsql cases from antlr-v4 repo

--- src/main/gen/PlSqlLexer.java
+++ src/main/gen/PlSqlLexer.java
@@ -1,4 +1,4 @@
-// Generated from /Users/cs79en/Developer/GitHub/antlr-grammars-plsql/src/main/antlr/name/jurgenei/parsers/PlSqlLexer.g4 by ANTLR 4.13.2
+// Generated from /Users/cs79en/Developer/GitHub/gradle/antlr-grammars-plsql/src/main/antlr/name/jurgenei/parsers/PlSqlLexer.g4 by ANTLR 4.13.2
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
